3e5be5178998df156bc6e3bd9edb452c15d9d2a9
Winner960111 <chenyih0111@gmail.com>
2025-04-14 00:40:21 +0900

    Add(master): Some other functionality

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+
+export default function About() {
+    return (
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <h5 className="bg-gray-400 text-red-500 font-bold">
+            This is About page</h5>
+      </div>
+    );
+  }
+  
