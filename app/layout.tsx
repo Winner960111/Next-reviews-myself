49fec16ecfc28d187174402144f75eaa4d7873c0
Winner960111 <chenyih0111@gmail.com>
2025-04-14 17:30:36 +0900

    Prefech(master): prefetch false functionality to improve the loading speed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,7 +14,19 @@ export default function RootLayout({
     <html lang="en">
       <body className="gap-10 flex flex-col">
         <header>
-          [Header]
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/about">About</Link>
+              </li>
+              <li>
+                <Link href="/reviews" prefetch = {false}>Reviews</Link>
+              </li>
+            </ul>
+          </nav>
         </header>
         <main>{children}</main>
         <footer>
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-'use client';
+// 'use client';
 
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(()=> {
-    window.alert("I'm Here")
-    console.log("Home page loaded");
-  })
+  // useEffect(()=> {
+  //   window.alert("I'm Here")
+  //   console.log("Home page loaded");
+  // })
   return (
     <div className="text-5xl">
         Hi how are you today?
