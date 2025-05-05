09c6731d211261e2c6daff2ccae3cc149fe82957
Winner960111 <chenyih0111@gmail.com>
2025-05-05 22:42:12 +0900

    Load file

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 // 'use client';
-
+import { readFile } from "node:fs/promises";
 // import { useEffect } from "react";
+import {orbitron} from "@/app/fonts"
 
-export default function Home() {
+export default async function Home() {
   // useEffect(()=> {
   //   window.alert("I'm Here")
   //   console.log("Home page loaded");
   // })
+  const text = await readFile("./content/readme.md", "utf-8");
   return (
-    <div className="text-5xl">
+    <div className={`text-5xl ${orbitron.className}`}>
         Hi how are you today?
+        {text}
     </div>
   );
 }
---
+++ content/readme.md
@@ -0,0 +1,10 @@
+# Heading
+## Sub-heading
+
+Rich text with _bold_ and _italic_.
+
+Another paragraph
+List:
+* file
+* image
+* audio
