8c9197eb4b50bc99c0d537ccba62b5bc500c05e6
Winner960111 <chenyih0111@gmail.com>
2025-06-09 18:07:42 +0900

    add(master): generate Static params for slugs

--- app/reviews/[slug]/page.tsx
+++ app/reviews/[slug]/page.tsx
@@ -1,12 +1,16 @@
 // 'use client';
 // import { useEffect } from "react";
 import { orbitron } from "@/app/fonts";
-import { getReview } from "@/lib/reviews";
+import { getReview, getSlugs } from "@/lib/reviews";
 
+export async function generateStaticParams() {
+  const slugs = await getSlugs();
+  return slugs.map((slug) => ({ slug }));
+}
 
 export default async function ReviewPage({params:{slug}}:{params:any}) {
      const review = await getReview(slug)
-
+     console.log("review:", slug);
   return (
     <>
       <div className={`text-5xl ${orbitron.className}`}>
--- lib/reviews.tsx
+++ lib/reviews.tsx
@@ -2,23 +2,28 @@ import { readdir, readFile } from "node:fs/promises";
 // import { useEffect } from "react";
 import matter from "gray-matter";
 import { marked } from "marked";
+import { get } from "node:http";
 
 export async function getReview(slug:string) {
   const text = await readFile(`./content/${slug}.md`, "utf-8");
   const { content, data:{ title, date } } = matter(text);
-  const body = marked(content, {headerIds: false, mangle: false});
+  const body = marked(content);
   return { title, date, body, slug}
 }
 
 export async function getReviews() {
-  const files = await readdir("./content/");
-  const slugs = files.filter((file) => file.endsWith(".md"))
-    .map((files)=> files.slice(0, -".md".length));
 
+  const slugs = await getSlugs();
   const reviews = [];
   for (const slug of slugs){
     const review = await getReview(slug);
     reviews.push(review);
   }
   return reviews 
+}
+
+export async function getSlugs() {
+  const files = await readdir("./content/");
+  return files.filter((file) => file.endsWith(".md"))
+    .map((files)=> files.slice(0, -".md".length));
 }
